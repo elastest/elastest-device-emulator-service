db5d6a50079ce1244150646e0055c479190f5ac5
sro <sro>
2017-07-10 11:23:11 +0200

    modification of eds

--- openmtc-gevent/src/openmtc_gevent/nscl_main.py
+++
@@ -1,3 +0,0 @@
-import openmtc_gevent.main
-
-openmtc_gevent.main.main("config-nscl.json", False)
--- server/openmtc-server/src/openmtc_server/platform/gevent/ServerRack.py
+++
@@ -1,6 +0,0 @@
-
-from futile.logging import get_logger
-
-get_logger(__name__).warning("Deprecated import")
-
-from openmtc_gevent.ServerRack import GEventServerRack
