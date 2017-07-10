08c58d00d0311370b6153dcda3373fd9ee68742f
sro <sro>
2017-07-10 17:18:43 +0200

    Tests added

---
+++ ZigBeeIPE/test/test_ZBSBase.py
@@ -0,0 +1,10 @@
+#from unittest import TestCase
+
+
+class TestZBSBase():
+    def test_parse(self):
+       # self.fail()
+        pass
+    def test_get_simulated_data(self):
+        #self.fail()
+        pass
--- ZigBeeIPE/test/test_zigBeeIPE.py
+++ ZigBeeIPE/test/test_zigBeeIPE.py
@@ -1,6 +1,5 @@
-
-
-from zigbeeipe.zig_bee_ipe import *
+#from unittest import TestCase
+from zigbeeipe.zig_bee_ipe import ZigBeeIPE
 from zigbeeipe.zig_bee_coordinator import *
 
 
@@ -8,43 +7,30 @@ class TestZigBeeIPE():
     def test__on_shutdown(self):
         self.usb_device = "/dev/ttyUSB0"
         self.coordinator = ZigBeeCoordinator(self.usb_device)
-        shut= self.coordinator.shutdown
+        shut = self.coordinator.shutdown
         assert shut
-
+        # pass
     def test__on_register(self):
         #self.fail()
         pass
-
     def test__create_device_container(self):
-    #   self.fail()
+        #self.fail()
         pass
-
-
     def test__register_device(self):
-    #   self.fail()
-         pass
-
-
+       # self.fail()
+        pass
     def test__handle_zbs110_switch(self):
-    #   self.fail()
+        #self.fail()
         pass
-
-
     def test__get_unified_data(self):
-    #   self.fail()
+        #self.fail()
         pass
-
-
     def test__push_content(self):
-    #   self.fail()
-         pass
-
-
+        #self.fail()
+        pass
     def test__handle_sensor_data(self):
-    #   self.fail()
-         pass
-
-
+        #self.fail()
+        pass
     def test__handle_device_data(self):
-    #   self.fail()
-         pass
+        #self.fail()
+        pass
