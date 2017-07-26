0243fefdfa67d47c8cee017de223972d6346497c
sro <sro>
2017-07-26 16:41:49 +0200

    Fixes for docker file

---
+++ eds/ipes/FrontEnd/test/test_frontEnd.py
@@ -0,0 +1,29 @@
+
+
+class TestFrontEnd():
+    def test__on_register(self):
+        #self.fail()
+        pass
+    def test_send_history(self):
+        self.fail()
+
+    def test_send_data(self):
+        self.fail()
+
+    def test_sensor_handler(self):
+        self.fail()
+
+    def test_handle_sensor(self):
+        self.fail()
+
+    def test_handle_sensors(self):
+        self.fail()
+
+    def test_handle_sensors_mems(self):
+        self.fail()
+
+    def test_send_motor_start(self):
+        self.fail()
+
+    def test_send_motor_stop(self):
+        self.fail()
