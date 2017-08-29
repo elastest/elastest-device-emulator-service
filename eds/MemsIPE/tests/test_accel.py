f852019981aa6236d6c71b73987595785b456aed
sro <sro>
2017-08-29 09:46:40 +0200

    $

---
+++ eds/MemsIPE/tests/test_accel.py
@@ -0,0 +1,33 @@
+#program for testing the acceleration in G-force I2C
+import smbus2
+import time
+import os
+
+
+class TestAccel():
+	def test_setUp(self):
+		pass
+
+	def test_getValueX_l(self):
+		pass
+
+	def test_getValueX_h(self):
+		pass
+
+	def test_getValueY_l(self):
+		pass
+
+	def test_getValueY_h(self):
+		pass
+
+	def test_getValueZ_l(self):
+		pass
+
+	def test_getValueZ_h(self):
+		pass
+
+	def test_getTempl(self):
+		pass
+
+	def test_getTemph(self):
+		pass
