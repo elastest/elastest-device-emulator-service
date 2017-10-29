f8a22d9dfa1b1ba3d80417947a2cb8c8ef54c4e0
sro <sro>
2017-10-29 09:27:46 +0100

    tempalte fixed

--- eds/FrontEnd/src/frontend/static/js/eds.js
+++ eds/FrontEnd/src/frontend/static/js/eds.js
@@ -1,42 +1,15 @@
 var configuredSensorList = {
-    'top-left': '/mn-cse-1/onem2m/ZigBeeIPE/devices/ZBS122S000001/sensor_data/brightness',
-    'top-middle': '/mn-cse-1/onem2m/ZigBeeIPE/devices/ZBS122S000001/sensor_data/humidity',
-    'top-right': '/mn-cse-1/onem2m/ZigBeeIPE/devices/ZBS122S000001/sensor_data/temperature',
-    'bottom-left':  '/mn-cse-1/onem2m/MemsIPE/sensor_data/x',
-    'bottom-middle': '/mn-cse-1/onem2m/MemsIPE/sensor_data/y',
-    'bottom-right': '/mn-cse-1/onem2m/MemsIPE/sensor_data/z'
+
+    'top-left':  '/mn-cse-1/onem2m/MemsIPE/sensor_data/x',
+    'top-middle': '/mn-cse-1/onem2m/MemsIPE/sensor_data/y',
+    'top-right': '/mn-cse-1/onem2m/MemsIPE/sensor_data/z'
 
 };
 
 
-function trafficLights(data) {
- //   var current = signals[(data.v)-1];
-    var current = signals[data];
-
-    if (current == 1)  {
-        $('.trafficlight').removeClass("aktiv");
-        $('#green').addClass("aktiv");
-    } else if  (current == 2){
-        $('.trafficlight').removeClass("aktiv");
-        $('#yellow').addClass("aktiv");
-    } else{
-        $('.trafficlight').removeClass("aktiv");
-        $('#red').addClass("aktiv");
-    }
-}
 
 
 
-$("#btnAdd1").on('click',function() {
-    console.log("Motor Started");
-    socket.emit("motor_start")
-
-
-});
-$("#btnAdd2").on('click',function() {
-    console.log("Motor Stopped");
-    socket.emit("motor_stop")
-});
 
 
 $(document).ready(function () {
