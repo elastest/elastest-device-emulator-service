ade721e8e6e6162d5626e05dfc96f90800b5452f
sro <sro>
2017-12-08 10:58:39 +0100

    frontend request send

--- eds/FrontEnd/exec-frontend.sh
+++ eds/FrontEnd/exec-frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sh openmtc-gevent/run_gscl &
-until $(curl --output /dev/null --silent --head --fail http://localhost:8000/onem2m); do
+until $(curl --output /dev/null --silent --head --fail http://localhost:6065/onem2m); do
   printf '.'
   sleep 5
 done
