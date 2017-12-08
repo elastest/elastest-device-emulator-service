ad60c348c303c900e302626ceec7669d8a808ff3
sro <sro>
2017-12-08 10:31:15 +0100

    http curl request for getting 200

--- eds/FrontEnd/exec-frontend.sh
+++ eds/FrontEnd/exec-frontend.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 sh openmtc-gevent/run_gscl &
-sleep 1
+until $(curl --output /dev/null --silent --head --fail http://localhost:8000/onem2m); do
+  printf '.'
+  sleep 5
+done
 sh frontend
