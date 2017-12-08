d2c535fa017a2d19ed7398a846920be9c6d4f092
sro <sro>
2017-12-08 13:39:01 +0100

    typo fixed

--- e2e-test/src/test/java/io/elastest/eds/test/e2e/EdsSupportServiceE2eTest.java
+++ e2e-test/src/test/java/io/elastest/eds/test/e2e/EdsSupportServiceE2eTest.java
@@ -56,7 +56,7 @@ public class EdsSupportServiceE2eTest extends EdsBaseTest {
         log.info("Navigate to TORM and start support service");
         driver.manage().window().setSize(new Dimension(1024, 1024));
         driver.manage().timeouts().implicitlyWait(5, SECONDS); // implicit wait
-        # navigate to the application home page
+        //navigate to the application home page
         driver.get("http://www.google.com")
         driver.get(tormUrl);
         startTestSupportService(driver, "EDS");
