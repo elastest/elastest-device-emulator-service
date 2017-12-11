82734ec1f322c803e960f50378b7052347bc8378
sro <sro>
2017-12-11 19:00:42 +0100

    execute frontend

--- eds/FrontEnd/exec-frontend.sh
+++ eds/FrontEnd/exec-frontend.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-sh openmtc-gevent/run_gscl &&
+. ../openmtc-gevent/run_gscl &&
 sleep 1
 #until $(curl --output /dev/null --silent --head --fail http://localhost:6065/); do
 #  printf '.'
 #  sleep 1
 #done
-sh frontend
+#sh frontend $@
+sh prep-env.sh
+PYTHONPATH=${PYTHONPATH}:src exec python -m frontend $@
