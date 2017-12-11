770e831fc2b63f20537b69ef2b3a6ff0970d2874
sro <sro>
2017-12-11 19:39:22 +0100

    Correction for gateway

--- eds/FrontEnd/exec-frontend.sh
+++ eds/FrontEnd/exec-frontend.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-. ../openmtc-gevent/run_gscl &&
+sh openmtc-gevent/run_gscl &
 sleep 1
 #until $(curl --output /dev/null --silent --head --fail http://localhost:6065/); do
 #  printf '.'
 #  sleep 1
 #done
-sh frontend $@
+sh frontend
