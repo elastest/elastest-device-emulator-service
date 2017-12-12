2e92136a20cdee904fecb31e83d1be88932de028
sro <sro>
2017-12-12 13:26:39 +0100

    driver capabilities added

--- e2e-test/src/test/java/io/elastest/eds/test/e2e/EdsSupportServiceE2eTest.java
+++ e2e-test/src/test/java/io/elastest/eds/test/e2e/EdsSupportServiceE2eTest.java
@@ -31,6 +31,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 
 import io.elastest.eds.test.base.EdsBaseTest;
+import io.github.bonigarcia.DriverCapabilities;
 import io.github.bonigarcia.SeleniumExtension;
 import java.io.BufferedReader;
 import java.io.InputStream;
