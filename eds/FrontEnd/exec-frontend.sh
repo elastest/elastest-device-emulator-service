a854b8de93d0e0a7a34b55fce6a74890ceea7200
sro <sro>
2017-12-19 17:28:16 +0100

    Removed --head from the curl

--- eds/FrontEnd/exec-frontend.sh
+++ eds/FrontEnd/exec-frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sh openmtc-gevent/run_gscl &
 sleep 1
-until $(curl --output /dev/null --silent --head --fail http://localhost:8000/); do
+until $(curl --output /dev/null --silent --fail http://localhost:8000/); do
   printf '.'
   sleep 1
 done
