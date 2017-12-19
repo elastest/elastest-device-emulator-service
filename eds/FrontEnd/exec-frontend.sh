6dd9e5f2b367e29c6801b1ca9da1d2940325cc35
sro <sro>
2017-12-19 17:06:24 +0100

    Testing for frontend

--- eds/FrontEnd/exec-frontend.sh
+++ eds/FrontEnd/exec-frontend.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sh openmtc-gevent/run_gscl &
 sleep 1
-#until $(curl --output /dev/null --silent --head --fail http://localhost:6065/); do
-#  printf '.'
-#  sleep 1
-#done
+until $(curl --output /dev/null --silent --head --fail http://localhost:8000/); do
+  printf '.'
+  sleep 1
+done
 sh frontend
