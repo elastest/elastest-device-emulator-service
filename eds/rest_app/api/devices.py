58b08927228e583c34e5b5ff7004040a05727420
sro <sro>
2018-02-20 15:05:09 +0100

    id field removed from devices

--- eds/rest_app/api/devices.py
+++ eds/rest_app/api/devices.py
@@ -1,15 +1,23 @@
 import datetime
 
 from connexion import NoContent
+import call
 
 devices = {}
-
+URL_BASE = 'http://172.18.0.5:8080/eds/devices'
+def get():
+    #id = str(id)
+    if call is None:
+        return NoContent, 404
+    print "this is 0",call
+    #return call
 
 def post(device):
     count = len(devices)
-    device['id'] = count + 1
+    name = str(name)
+    #device['id'] = count + 1
     device['registered'] = datetime.datetime.now()
-    devices[device['id']] = device
+    devices[device['name']] = device
     return device, 201
 
 
@@ -30,15 +38,16 @@ def delete(id):
     return NoContent, 204
 
 
-def get(id, device):
-    id = str(id)
-    if devices.get(id) is None:
-        return NoContent, 404
-    devices[id] = device
-
-    return devices[id]
+# def get(id, device):
+#     id = str(id)
+#     if devices.get(id) is None:
+#         return NoContent, 404
+#     devices[id] = device
+#
+#     return devices[id]
 
 
 def search():
     # NOTE: we need to wrap it with list for Python 3 as dict_values is not JSON serializable
-    return list(devices.values())
+   # return list(devices.values())
+  return devices.values()
