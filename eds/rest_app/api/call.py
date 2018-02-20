ced252b124f3beaa13f839bff15ebbbf403a0980
sro <sro>
2018-02-20 14:46:43 +0100

    api call added

--- eds/rest_app/api/call.py
+++ eds/rest_app/api/call.py
@@ -7,30 +7,38 @@ import json
 from base64 import b64decode
 
 # Replace with the correct URL
-url = "http://172.18.0.2:8000/onem2m/ZigBeeIPE/devices/ZBS122S000001/sensor_data/brightness/latest"
+url1 = "http://172.18.0.2:8000/onem2m/MemsIPE/sensor_data/x/latest/"
+url2 = "http://172.18.0.2:8000/onem2m/MemsIPE/sensor_data/y/latest/"
+url3 = "http://172.18.0.2:8000/onem2m/MemsIPE/sensor_data/z/latest/"
+
+url={url1, url2, url3}
+
+print len(url)
 
 # It is a good practice not to hardcode the credentials. So ask the user to enter credentials at runtime
 #myResponse = requests.get(url, auth=HTTPDigestAuth(raw_input("username: "), raw_input("Password: ")), verify=True)
-myResponse = requests.get(url)
-print "code:"+ str(myResponse.status_code)
+for i in url:
+	#print i
+	myResponse = requests.get(i)
+	print "code:"+ str(myResponse.status_code)
 #print myResponse.text
 
 
 # For successful API call, response code will be 200 (OK)
-if (myResponse.ok):
+	if (myResponse.ok):
 
 	# Loading the response data into a dict variable
 	# json.loads takes in only binary or string variables so using content to fetch binary content
 	# Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
 	#jData = json.loads(myResponse.content)
-	jData = myResponse.content
-	#print "this is ", jData
+		jData = myResponse.content
+		#print "this is ", jData
 
-	#print len(jData)
+		#print len(jData)
 
-	array = json.loads(jData)
-	#print array["m2m:cin"]["con"]
-	convert = b64decode(array["m2m:cin"]["con"])
+		array = json.loads(jData)
+		#print array["m2m:cin"]["con"]
+		convert = b64decode(array["m2m:cin"]["con"])
 	print convert
 
 else:
