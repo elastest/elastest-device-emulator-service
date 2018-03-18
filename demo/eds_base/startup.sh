f36ade1614d2b547d6bbc2de56a7da7ee5f37e1e
Varun Gowtham <v.gowtham@tu-berlin.de>
2018-03-18 14:07:42 +0100

    full integration of eds-base

--- demo/eds_base/startup.sh
+++ demo/eds_base/startup.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
+openmtc_path=/usr/local/src/openmtc
 # create the EDS Orchestrator app
 ./create-app-structure -d -a eds_orch EDSOrch
 # copy the relevant files to the application
+cp /tmp/EDSOrch/__init__.py ${openmtc_path}/apps/EDSOrch/src/eds_orch/
+cp /tmp/EDSOrch/eds_orch.py ${openmtc_path}/apps/EDSOrch/src/eds_orch/
 
 # create Temperature Sensor app
 ./create-app-structure -d TemperatureSensor
+cp /tmp/TemperatureSensor/__init__.py ${openmtc_path}/apps/TemperatureSensor/src/temperaturesensor/
+cp /tmp/TemperatureSensor/temperature_sensor.py ${openmtc_path}/apps/TemperatureSensor/src/temperaturesensor/
+
 
 # create Simple Actuator app
 ./create-app-structure -d SimpleActuator
+cp /tmp/SimpleActuator/__init__.py ${openmtc_path}/apps/SimpleActuator/src/simpleactuator/
+cp /tmp/SimpleActuator/simple_actuator.py ${openmtc_path}/apps/SimpleActuator/src/simpleactuator/
 
-sh /usr/local/bin/configure-and-start &
+
+sh /usr/local/src/openmtc/openmtc-gevent/configure-and-start &
 
 sleep 1
 
@@ -19,4 +28,4 @@ until $(curl --output /dev/null --silent --fail http://localhost:8000/onem2m); d
 	sleep 1
 done
 
-sh
+sh ${openmtc_path}/apps/eds_orch
