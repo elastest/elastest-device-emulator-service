8226ce331d0fadb7d23c8610d34bbd6b1741f37c
Varun Gowtham <v.gowtham@tu-berlin.de>
2018-03-18 08:52:12 +0100

    restructure eds-base

--- demo/eds_base/SimpleActuator/src/simpleactuator/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-TODO: Add description here
-"""
-
-__version__ = "ADD-VERSION-HERE"
-__description__ = "SimpleActuator"
-__author_name__ = "Varun Gowtham"
-__author_mail__ = "v.gowtham@tu-berlin.de"
-__requires__ = []
--- demo/eds_base/TemperatureSensor/src/temperaturesensor/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-TODO: Add description here
-"""
-
-__version__ = "ADD-VERSION-HERE"
-__description__ = "TemperatureSensor"
-__author_name__ = "ADD_AUTHOR_HERE"
-__author_mail__ = "ADD_MAIL_HERE"
-__requires__ = []
