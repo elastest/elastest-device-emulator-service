99f1e3af82d03b39134bd0faed2a8ae164464336
sro <sro>
2018-04-18 16:17:09 +0200

    getting device data via api

--- eds/rest_app/api/devices.py
+++ eds/rest_app/api/devices.py
@@ -1,12 +1,20 @@
 import datetime
-
+import requests
+import json
+from base64 import b64decode
 from connexion import NoContent
 
-devices = {}
-
-
+raw_data = requests.get("http://172.18.0.2:8000/onem2m/MemsIPE/sensor_data/x/latest/")
+array = raw_data.content
+print array
+final_data = json.loads(array)
+print final_data
+devices = b64decode(final_data["m2m:cin"]["con"])
+print devices
+#devices= final_data
 def post(device):
     count = len(devices)
+    print(count)
     device['id'] = count + 1
     device['registered'] = datetime.datetime.now()
     devices[device['id']] = device
@@ -30,14 +38,14 @@ def delete(id):
     return NoContent, 204
 
 
-def get(id):
-    id = int(id)
-    if devices.get(id) is None:
+def get(name):
+    name = str(name)
+    if devices.get(name) is None:
         return NoContent, 404
 
-    return devices[id]
+    return devices[name]
 
-
+#
 def search():
     # NOTE: we need to wrap it with list for Python 3 as dict_values is not JSON serializable
-    return list(devices.values())
+   return devices
