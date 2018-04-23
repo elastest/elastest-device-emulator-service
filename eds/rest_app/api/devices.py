230472e32278049316a7b4cb4793321cbbbef331
sro <sro>
2018-04-23 17:11:57 +0200

    little modification in api

--- eds/rest_app/api/devices.py
+++ eds/rest_app/api/devices.py
@@ -1,9 +1,10 @@
 import datetime
 import requests
-from flask import Flask, render_template, flash, request
-import simplejson
+import json
+#import connexion
 from base64 import b64decode
 from connexion import NoContent
+import logging
 import itertools
 
 
@@ -15,11 +16,10 @@ url = {url1, url2, url3}
 
 res = [requests.get(i) for i in url]
 d = [j.content for j in res]
-
-dic = [simplejson.loads(q) for q in d]
-#print dic
+dic = [json.loads(q) for q in d]
 raw_data = [m["m2m:cin"]["con"] for m in dic]
-registered = [k["m2m:cin"]["ct"] for k in dic]
+regis = [k["m2m:cin"]["ct"] for k in dic]
+registered = str(regis[0])
 data = [de.encode("utf-8") for de in raw_data]
 
 convert =[b64decode(pa) for pa in data]
@@ -33,10 +33,8 @@ c = convert[2]
 
 devices = {}
 
-app = Flask(__name__)
 
-devices['registered'] = datetime.datetime.now()
-devices['servicename'] ='' #'MemsIPE'  #request.form['servicename']
+
 mems = {}
 axis = {}
 
@@ -48,15 +46,13 @@ axis['z']= a
 mems['id']= next(itertools.count(100))
 mems['axis']= axis
 devices['mems']= mems
+devices['registered'] = registered
+devices['servicename'] = 'MemsIPE' #'MemsIPE'  #request.form['servicename']
 
+print devices
 
-def post(servicename):
-    count = len(devices)
-    print(count)
-    servicename['id'] = count + 1
-    servicename['registered'] = datetime.datetime.now()
-    devices[servicename['id']] = servicename
-    return servicename, 201
+def get_all(servicename=None):
+    return [device for device in devices.values() if not servicename or device['servicename'] == servicename]
 
 
 def put(id, servicename):
@@ -69,19 +65,16 @@ def put(id, servicename):
 
 
 def delete(id):
-    id = str(id)
-    if devices.get(id) is None:
+    if id in devices:
+        logging.info('Deleting device %s..', id)
+        del devices[id]
+        return NoContent, 204
+    else:
         return NoContent, 404
-    del devices[id]
-    return NoContent, 204
 
-
-def get(id, servicename):
-    id = str(id)
-    if devices.get(id) is None:
-        return NoContent, 404
-    devices[id] = servicename
-    return devices[id]
+def get(id):
+    device = devices.get(id)
+    return device or ('Not found', 404)
 
 def search():
     return devices
