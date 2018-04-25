bec8a3a4da523159112b0fbd531b1d4e549cbd02
sro <sro>
2018-04-25 11:28:49 +0200

    unittest framework removed

--- tjob-test/tjob_eds.py
+++ tjob-test/tjob_eds.py
@@ -1,34 +1,31 @@
 from selenium import webdriver
-
+import sys
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 import selenium
 from time import sleep
-import unittest
+#import unittest
 
 
 
+#open nightly and go to test support services
+url = sys.argv[1]
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+driver = webdriver.Chrome(chrome_options=options)
+driver.get(url)
 
 
-class Tjobs(unittest.TestCase):
+driver.implicitly_wait(20)
+driver.maximize_window()
 
-
-	#open nightly and go to test support services
-	def Test_job1(self):
-		#show the location of web driver locally
-		#self.driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-
-		url = sys.argv[1]
-		self.driver.implicitly_wait(20)
-		self.driver.maximize_window()
-
-		projectname = 'tjobs_eds_1'
-		tjobname = 'test_tjobs'
-		#env_docker_image = 'ubuntu:16.04'
-		env_docker_image = 'rowshan/eds_e2e'
-		commands = """
+projectname = 'tjobs_eds_1'
+tjobname = 'test_tjobs'
+#env_docker_image = 'ubuntu:16.04'
+env_docker_image = 'rowshan/eds_e2e'
+commands = """
 		git clone https://github.com/rowshan/eds_e2e.git
 		cd eds_e2e
 		docker build -t rowshan/eds_e2e .
@@ -37,76 +34,71 @@ class Tjobs(unittest.TestCase):
 		"""
 
 
-		options = webdriver.ChromeOptions()
-		options.add_argument('headless')
-		driver = webdriver.Chrome(chrome_options=options)
-		# self.driver = webdriver.Chrome()
 
-		#self.driver.set_page_load_timeout(1000)
-		#self.driver.get("http://nightly.elastest.io:37000")
-		self.driver.get(url)
-		assert self.driver.title, 'Elastest Nightly Home'
-		sleep(1)
+#driver = webdriver.Chrome()
+
+#self.driver.set_page_load_timeout(1000)
+#self.driver.get("http://nightly.elastest.io:37000")
+driver.get(url)
+assert driver.title, 'Elastest Nightly Home'
+sleep(1)
 
 
-		### navigate to the project in the side_nav
-		project = self.driver.find_element_by_id('nav_projects')
+### navigate to the project in the side_nav
+project = driver.find_element_by_id('nav_projects')
 
-		if not project.is_displayed():
-			web_element = self.driver.find_element_by_id("main_menu").click()
-			sleep(1)  # delay to allow menu animation to complete.
-			project.click()
-		sleep(1)
+if not project.is_displayed():
+	web_element = driver.find_element_by_id("main_menu").click()
+	sleep(1)  # delay to allow menu animation to complete.
+	project.click()
+sleep(1)
 
-		# create new project and save it
-		self.driver.find_element_by_xpath("//button[contains(string(), 'New Project')]").click()
-		self.driver.find_element_by_name("project.name").send_keys(projectname)
-		self.driver.find_element_by_xpath("//button[contains(string(), 'SAVE')]").click()
-		sleep(1)
-
-
-		####Create a tjobs
-		self.driver.find_element_by_xpath("//button[contains(string(), 'New TJob')]").click()
-		self.driver.find_element_by_name("tJobName").send_keys(tjobname)
-		self.driver.find_element_by_class_name("mat-select-trigger").click()
-		self.driver.find_element_by_xpath("//md-option[contains(string(), 'None')]").click()
-		#self.driver.save_screenshot('ETM_tjobs_eds.png')
-
-		self.driver.find_element_by_name("tJobImageName").send_keys(env_docker_image)
-	#	self.driver.find_element_by_name("mat-input-infix")
-		self.driver.find_element_by_id("commands").send_keys(commands)
-		self.driver.find_element_by_xpath("//md-checkbox[@title='Select EDS']").click()
-		self.driver.find_element_by_xpath("//button[contains(string(), 'SAVE')]").click()
-		sleep(1)
-
-		# run tjob
-		self.driver.find_element_by_xpath("//button[@title='Run TJob']").click()
-		sleep(1)
-		self.driver.save_screenshot('ETM_run_eds_tjobs.png')
-		sleep(1)
-
-		# check job status.
-		while True:
-			try:
-				check_job_status = self.driver.find_element_by_xpath(
-					"//etm-dashboard/div[1]/div/md-card/md-card-content/div/span[1]/span[1][ contains(string(), 'SUCCESS') or contains(string(), 'ERROR') or contains(string(), 'FAIL') ]")
-				print(check_job_status.text)
-				break
-			except selenium.common.exceptions.NoSuchElementException:
-				print("waiting to finish ...")
-				sleep(20)
-
-		if 'SUCCESS' in check_job_status.text:
-			print('successfull')
-			print(check_job_status.text)
-			exit(0)
-		else:
-			print('Failed')
-			print(check_job_status.text)
-			exit(1)
-		self.driver.close()
+# create new project and save it
+driver.find_element_by_xpath("//button[contains(string(), 'New Project')]").click()
+driver.find_element_by_name("project.name").send_keys(projectname)
+driver.find_element_by_xpath("//button[contains(string(), 'SAVE')]").click()
+sleep(1)
 
 
-if __name__ == "__main__":
-	unittest.main()#(verbosity=2)[/python]
+####Create a tjobs
+driver.find_element_by_xpath("//button[contains(string(), 'New TJob')]").click()
+driver.find_element_by_name("tJobName").send_keys(tjobname)
+driver.find_element_by_class_name("mat-select-trigger").click()
+driver.find_element_by_xpath("//md-option[contains(string(), 'None')]").click()
+#self.driver.save_screenshot('ETM_tjobs_eds.png')
+
+driver.find_element_by_name("tJobImageName").send_keys(env_docker_image)
+#	self.driver.find_element_by_name("mat-input-infix")
+driver.find_element_by_id("commands").send_keys(commands)
+driver.find_element_by_xpath("//md-checkbox[@title='Select EDS']").click()
+driver.find_element_by_xpath("//button[contains(string(), 'SAVE')]").click()
+sleep(1)
+
+# run tjob
+driver.find_element_by_xpath("//button[@title='Run TJob']").click()
+sleep(1)
+driver.save_screenshot('ETM_run_eds_tjobs.png')
+sleep(1)
+
+# check job status.
+while True:
+	try:
+		check_job_status = driver.find_element_by_xpath(
+			"//etm-dashboard/div[1]/div/md-card/md-card-content/div/span[1]/span[1][ contains(string(), 'SUCCESS') or contains(string(), 'ERROR') or contains(string(), 'FAIL') ]")
+		print(check_job_status.text)
+		break
+	except selenium.common.exceptions.NoSuchElementException:
+		print("waiting to finish ...")
+		sleep(20)
+
+if 'SUCCESS' in check_job_status.text:
+	print('successfull')
+	print(check_job_status.text)
+	exit(0)
+else:
+	print('Failed')
+	print(check_job_status.text)
+	exit(1)
+driver.close()
+
 
